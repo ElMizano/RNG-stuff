f3cf24cc86563441f686a6cb3467a50c676e9e72
MathiSuS <fanigliulo.mathis@gmail.com>
2022-04-13 15:01:55 +0200

    commit a few bitches

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ Created on Wed Apr 13 14:37:35 2022
 
 @author: Xavier & Mathis
 """
+from random import randint
 
 class Random:
     
-    __init__(self):
-        
+    def __init__(self, size = 100):
+        self.matrice = [randint(0, 1024) for i in range(size)]
         return
-        
+    
+    def __repr__(self):
+        return str(self.matrice)
+
+Matrice_test = Random()
+print(Matrice_test)
