731077ff37917167d0cdd59e3741fcca42c76fbc
Mizano <xavier.ferber@gmail.com>
2022-04-13 15:03:10 +0200

    Created GraphWhiteNoise

--- main.py
+++ main.py
@@ -4,10 +4,23 @@ Created on Wed Apr 13 14:37:35 2022
 
 @author: Xavier & Mathis
 """
+import numpy as np
+import matplotlib.pyplot as plt
 
-class Random:
+class Matrice(size = 100):
     
     __init__(self):
         
         return
-        
+
+def GraphWhiteNoise(whitenoise):
+    graph = np.linspace(0, len(whitenoise), len(whitenoise), endpoint=True)
+    plt.plot(graph, whitenoise, color="black", linewidth=2.5, linestyle="-",)
+    
+    plt.legend(loc='upper left', frameon=True)
+    plt.show()
+
+GraphWhiteNoise([15,18,100,500,3,231,144])
+
+
+    
