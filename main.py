f2c80f7c5e201aef98aae5ff4925bed2f539e484
Mizano <xavier.ferber@gmail.com>
2022-04-13 15:34:05 +0200

    Merged the stuff

--- main.py
+++ main.py
@@ -4,36 +4,28 @@ Created on Wed Apr 13 14:37:35 2022
 
 @author: Xavier & Mathis
 """
-<<<<<<< HEAD
 import numpy as np
 import matplotlib.pyplot as plt
-=======
 from random import randint
->>>>>>> origin/main
 
-class Matrice(size = 100):
+
+class Matrice:
     
     def __init__(self, size = 100):
         self.matrice = [randint(0, 1024) for i in range(size)]
         return
-<<<<<<< HEAD
-
-def GraphWhiteNoise(whitenoise):
-    graph = np.linspace(0, len(whitenoise), len(whitenoise), endpoint=True)
-    plt.plot(graph, whitenoise, color="black", linewidth=2.5, linestyle="-",)
     
-    plt.legend(loc='upper left', frameon=True)
-    plt.show()
-
-GraphWhiteNoise([15,18,100,500,3,231,144])
 
 
-    
-=======
+    def GraphWhiteNoise(self):
+        graph = np.linspace(0, len(self.matrice), len(self.matrice), endpoint=True)
+        plt.plot(graph, self.matrice, color="black", linewidth=2.5, linestyle="-",)
+        
+        plt.legend(loc='upper left', frameon=True)
+        plt.show()
     
     def __repr__(self):
         return str(self.matrice)
 
-Matrice_test = Random()
-print(Matrice_test)
->>>>>>> origin/main
+matrice = Matrice(size = 150)
+print(matrice.GraphWhiteNoise())
